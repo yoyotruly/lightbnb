a69240024f2289304906078b28714116ba7ce777
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-11 19:31:39 -0400

    Fix sql format

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -1,14 +1,14 @@
 WITH property_avg_rating AS (
-	SELECT property_id,
-         AVG(rating) AS average_rating
-    FROM property_reviews
-   GROUP BY property_id
+    SELECT property_id,
+           AVG(rating) AS average_rating
+      FROM property_reviews
+     GROUP BY property_id
 )
 
 SELECT p.id,
        p.title,
-	     p.cost_per_night,
-	     r.average_rating
+	 p.cost_per_night,
+	 r.average_rating
   FROM properties p
        JOIN property_avg_rating r ON p.id = r.property_id
  WHERE city LIKE '%Vancouver%'
