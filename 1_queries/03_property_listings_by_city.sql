c782039f41ed042bbb90e962a1d5452d33cde720
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-11 19:17:07 -0400

    Add property listing by city query

---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,17 @@
+WITH property_avg_rating AS (
+	SELECT property_id,
+         AVG(rating) AS average_rating
+    FROM property_reviews
+   GROUP BY property_id
+)
+
+SELECT p.id,
+       p.title,
+	     p.cost_per_night,
+	     r.average_rating
+  FROM properties p
+       JOIN property_avg_rating r ON p.id = r.property_id
+ WHERE city LIKE '%Vancouver%'
+       AND average_rating >= 4
+ ORDER BY cost_per_night
+ LIMIT 10;
